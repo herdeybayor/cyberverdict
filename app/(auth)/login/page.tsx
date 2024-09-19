aab444aa4efe29818a94a7c551b703ce18187c41
herdeybayor <shereefdeen4real@gmail.com>
2024-09-19 22:25:34 +0100

    refactor: update login left side quote

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,12 +8,15 @@ export default function LoginPage() {
                 <Image src="/images/book-stack.png" className="-z-10" fill alt="" />
                 <div className="backdrop-blur-sm bg-primary/50 w-full h-full absolute inset-0 -z-10" />
                 <div className="text-white">
+                    <Image src="/images/quote.png" width={24} height={24} alt="" className="absolute -ml-6 -mt-11" />
                     <p className="max-w-96 text-lg leading-[1.72]">
                         Those people who develop the ability to continuously acquire new and better forms of knowledge that they can apply to their work and to their lives will be the movers and
                         shakers in our society for the indefinite future
                     </p>
 
                     <span className="font-medium mt-16 inline-block">Elon Musk</span>
+
+                    <span className="block w-10 h-8 border-white border-r-[14px] border-b-[14px] ml-auto -mr-5 mt-4" />
                 </div>
             </div>
             <div className="h-full flex-1">
